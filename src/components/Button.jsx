d0a9ec37a20bbae3a3cabeb35c8e3cf1dc4248db
kwojtas172 <k.wojtas172@gmail.com>
2021-08-26 13:41:39 +0200

    created fn showButton - first step: position of mouse / button

--- src/components/Button.jsx
+++ src/components/Button.jsx
@@ -1,8 +1,21 @@
 import React from 'react';
 
 const Button = () => {
+
+    document.addEventListener('mousemove', e => {
+        // console.log(e.screenX, e.screenY);
+        showButton(e)
+    })
+
+    const showButton = e => {
+        const mouseX = e.clientX;
+        const mouseY = e.clientY;
+        const btn = document.getElementById('btn');
+        console.log(mouseX, mouseY, btn.getBoundingClientRect());
+    }
+
     return (
-        <button className='btn'>Kliknij mnie!</button>
+        <button className='btn' id='btn'>Kliknij mnie!</button>
     );
 }
 
