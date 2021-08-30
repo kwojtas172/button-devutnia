772702032e06f5946773b364cfdb76b4c912c031
kwojtas172 <k.wojtas172@gmail.com>
2021-08-30 14:56:31 +0200

    corrected fetch - for sleeping on heroku

--- src/components/Info.jsx
+++ src/components/Info.jsx
@@ -7,12 +7,19 @@ const Info = () => {
     const [dataFromFetch, setDataFromFetch] = React.useState(null);
 
     React.useEffect(() => {
-        fetch('https://btn-shu-api.herokuapp.com/')
-        .then(res => res.json())
-        .then(data => {
-            setDataFromFetch(data)
-        })
-        .catch(err => console.log(err))
+        const fetchFromApi = () => {
+            fetch('https://btn-shu-api.herokuapp.com/')
+            .then(res => res.json())
+            .then(data => {
+                setDataFromFetch(data)
+            })
+            .catch(err => {
+                console.log(err);
+                fetchFromApi()
+            })
+            };
+            
+            fetchFromApi()
     }, [])
 
     return (
