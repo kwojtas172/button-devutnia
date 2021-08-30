15ebe1d6ebbdeabd20eed0073f4b642b0bb7c877
kwojtas172 <k.wojtas172@gmail.com>
2021-08-30 10:22:53 +0200

    cleared code in component Button

--- src/components/Button.jsx
+++ src/components/Button.jsx
@@ -10,47 +10,47 @@ const Button = ({handleShowButton}) => {
             const mouseX = e.clientX;    
             const mouseY = e.clientY;
             const btn = document.getElementById('btn');
-            const btnPosition = btn? btn.getBoundingClientRect() : null;
-            handleBtnNameX(mouseX, btnPosition);
-            handleBtnNameY(mouseY, btnPosition);
+            const btnPosition = btn?.getBoundingClientRect();
+            if(btnPosition) {
+                handleBtnNameX(mouseX, btnPosition);
+                handleBtnNameY(mouseY, btnPosition);
+            }
         }
 
     React.useEffect(() => {
         if(!isOff) {
             document.addEventListener('mousemove', showButton);
-        } else document.removeEventListener('mousemove', showButton)
-    }, []) // eslint-disable-line react-hooks/exhaustive-deps
+        } else {
+            document.removeEventListener('mousemove', showButton);
+            handleShowButton();
+        }
+    }, [isOff]) // eslint-disable-line react-hooks/exhaustive-deps
 
     const handleBtnNameX = (mouseX, btnPosition) => {
-        if(btnPosition) {
-            if(mouseX < btnPosition.left) {
-                setBtnNameX('w prawo')
-            } else if(mouseX > btnPosition.right) {
-                setBtnNameX('w lewo')
-            } else {
-                setBtnNameX('');
-            }
+        if(mouseX < btnPosition.left) {
+            setBtnNameX('w prawo');
+        } else if(mouseX > btnPosition.right) {
+            setBtnNameX('w lewo');
+        } else {
+            setBtnNameX('');
         }
     }
 
     const handleBtnNameY = (mouseY, btnPosition) => {
-        if(btnPosition) {
-            if(mouseY < btnPosition.top) {
-                setBtnNameY('w dół')
-            } else if(mouseY > btnPosition.bottom) {
-                setBtnNameY('w górę')
-            } else setBtnNameY('')
-        }
+        if(mouseY < btnPosition.top) {
+            setBtnNameY('w dół');
+        } else if(mouseY > btnPosition.bottom) {
+            setBtnNameY('w górę');
+        } else setBtnNameY('');
        
     }
 
     const showMe = () => { 
-        setIsOff(true)
-        handleShowButton()
+        setIsOff(true);
     }
 
     return (
-        <button className='btn' id='btn' onClick={showMe}>{(btnNameX || btnNameY) ? `Przesuń myszką ${(btnNameX === true || btnNameY) === true ? `` : `${btnNameX} ${btnNameY}`}` : 'Kliknij mnie!'}</button>
+        <button className='btn' id='btn' onClick={showMe}>{(btnNameX || btnNameY) ? `Przesuń myszką ${(btnNameX === true || btnNameY === true ) ? `` : `${btnNameX} ${btnNameY}`}` : 'Kliknij mnie!'}</button>
     );
 }
 
